dd4df9304c3e5d020b5bcf6d154ebdc846a571bc
Jose Manuel Franco <manezgz@hotmail.com>
2015-07-07 21:29:14 +0200

    Initial Commit

---
+++ HackerBooksCoreData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerBooksCoreData
+//
+//  Created by Jose Manuel Franco on 7/7/15.
+//  Copyright (c) 2015 Jose Manuel Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
