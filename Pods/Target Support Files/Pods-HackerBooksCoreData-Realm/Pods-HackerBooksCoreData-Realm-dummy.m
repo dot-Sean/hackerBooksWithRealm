635544d7c9f2df1a2d1142343025f1e6f961a38a
Jose Manuel Franco <manezgz@hotmail.com>
2015-08-07 00:56:42 +0200

    Fixes #19 Add MapKit

---
+++ Pods/Target Support Files/Pods-HackerBooksCoreData-Realm/Pods-HackerBooksCoreData-Realm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackerBooksCoreData_Realm : NSObject
+@end
+@implementation PodsDummy_Pods_HackerBooksCoreData_Realm
+@end
---
+++ Pods/Target Support Files/Pods-HackerBooksCoreData/Pods-HackerBooksCoreData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackerBooksCoreData : NSObject
+@end
+@implementation PodsDummy_Pods_HackerBooksCoreData
+@end
